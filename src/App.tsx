f90416583788a9643c7b2a3565881dd126c5e22d
Hashib Raja <hashibraja23@gmail.com>
2023-09-25 19:37:10 +0530

    Render static data list

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+type ListI = {
+  description: string;
+  quantity: number;
+  packed: boolean;
+  id: number;
+};
+
+const intialData: ListI[] = [
+  { description: "hello hashib", quantity: 10, packed: true, id: 1 },
+  { description: "hello hashib", quantity: 10, packed: false, id: 2 },
+  { description: "hello hashib", quantity: 10, packed: false, id: 3 },
+];
+
 function App() {
   return (
     <div className="app">
@@ -17,12 +30,41 @@ function Form() {
   return (
     <div className="add-form">
       <h3>What do you need for your trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((ele) => (
+          <option key={ele} value={ele}>
+            {ele}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="text..." />
+      <button>Add</button>
     </div>
   );
 }
 
 function PackagingList() {
-  return <div className="list">list</div>;
+  return (
+    <div className="list">
+      <ul>
+        {intialData.map((ele) => (
+          <Item {...ele} key={ele.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ description, quantity, packed, id }: ListI) {
+  console.log(description, quantity, packed, id);
+  return (
+    <li>
+      <span style={packed ? { textDecoration: "line-through" } : {}}>
+        {quantity} {description}
+      </span>
+      <span>❌</span>
+    </li>
+  );
 }
 
 function Stats() {

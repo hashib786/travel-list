6e02dfba2a7b9753f20d03ad15fdc33854763197
Hashib Raja <hashibraja23@gmail.com>
2023-09-26 12:07:56 +0530

    Implemented sorted functionality in our app

--- src/App.tsx
+++ src/App.tsx
@@ -95,13 +95,33 @@ interface PackingInterface {
   onToggle: (id: number) => void;
 }
 function PackagingList({ item, onDelete, onToggle }: PackingInterface) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItem: ListI[] = [];
+  if (sortBy === "input") sortedItem = item;
+
+  if (sortBy === "description")
+    sortedItem = item
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packing")
+    sortedItem = item.sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {item.map((ele) => (
+        {sortedItem.map((ele) => (
           <Item {...ele} key={ele.id} onDelete={onDelete} onToggle={onToggle} />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input</option>
+          <option value="description">Sort By Description</option>
+          <option value="packing">Sort By Packing</option>
+        </select>
+      </div>
     </div>
   );
 }

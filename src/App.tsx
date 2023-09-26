4a05322c7ab4d644604b4cdb89a0e3e0ca1e3c06
Hashib Raja <hashibraja23@gmail.com>
2023-09-26 09:16:19 +0530

    Implemented stats placeholder value

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
         onDelete={handleDelete}
         onToggle={handleToggleItem}
       />
-      <Stats />
+      <Stats item={item} />
     </div>
   );
 }
@@ -138,10 +138,27 @@ function Item({
   );
 }
 
-function Stats() {
+function Stats({ item }: { item: ListI[] }) {
+  const numItems = item.length;
+  const numPacked = item.reduce((acc, item) => (acc += item.packed ? 1 : 0), 0);
+
+  if (!numItems)
+    return (
+      <footer className="stats">
+        <em>Please add some items</em>
+      </footer>
+    );
+
+  const percentage = (numPacked / numItems) * 100;
+
   return (
     <footer className="stats">
-      <em>You have X item in your list, and already packed X(X%)</em>
+      <em>
+        {percentage === 100
+          ? `You completed all packing 📦📦`
+          : `You have ${numItems} item in your list, and already packed ${numItems}(
+        ${percentage}%)`}
+      </em>
     </footer>
   );
 }
